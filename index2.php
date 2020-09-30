f02ece0201a0f80288e20b1edc57dc6bd5ec528b
Moritz 'e1mo' Fromm <git@e1mo.de>
2020-09-30 23:19:34 +0200

    Add basic Sendmail functionality

--- index2.php
+++ index2.php
@@ -1,8 +1,32 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $default_config = require('config.default.php');
 $config = require('config.php');
 $config = array_merge($default_config, $config);
+$message = '';
+$mailSuccessCode = true;
+
+if (isset($_POST['message'])) {
+    $message .= 'Message on '.date('c') . ":\n\n";
+    $message .= htmlentities($_POST['message']);
+    // In case any of our lines are larger than 70 characters, we should use wordwrap()
+    $message = wordwrap($message, 70, "\r\n");
+
+    $toUser = $config['owner']['e-mail'];
+    $headers['From'] = $config['message']['sender'];
+    $headers['Mime-Version'] = '1.0';
+    $headers['Content-type'] = 'text/plain; charset=UTF-8';
+
+    $mailSuccessCode = mail($toUser, $config['message']['subject'], $message, $headers);
+    if ($mailSuccessCode) {
+        $mailResponse = 'Your message was sent successfully.';
+    } else {
+        $mailResponse = 'There was an error during the sending of the E-Mail.';
+    }
+}
 
 ?>
 
@@ -16,76 +40,93 @@ $config = array_merge($default_config, $config);
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1><?php echo $config['page']['title']; ?></h1>
-    <p><?php echo $config['page']['description']; ?></p>
+    <div class="content">
+        <?php
+            if (isset($mailResponse)) {
+                if ($mailSuccessCode) {
+                    $base = '<div class="success">%s</div>';
+                } else {
+                    $base = '<div class="error">%s</div>';
+                }
 
-    <form action="" method="post">
-        <?php
-        if (!empty($config['form']['message']['label'])) {
-?><label for="message"><?php echo $config['form']['message']['label']; ?></label><br>
-<?php
-        }
+                printf($base, $mailResponse);
+            }
         ?>
-        <textarea
-            name="message"
-            id="message"
-            required
-            autofocus
-            placeholder="<?php
-                echo $config['form']['message']['placeholder'];
-            ?>"
-        ></textarea>
-        <br>
-        <p><?php
-            echo $config['form']['sender']['label'];
-        ?></p>
-        <div class="flex-container">
-            <div class="item">
-        <?php
-        if (!empty($config['form']['sender']['name']['label'])) {
-?><label for="sender-name"><?php echo $config['form']['sender']['name']['label']; ?></label><br>
-<?php
-        }
-        ?>
-        <input
-            type="text"
-            name="sender-name"
-            id="sender-name"
-            placeholder="<?php
-                echo $config['form']['sender']['name']['placeholder'];
-            ?>"
-        >
-            </div>
-            <div class="item">
-        <?php
-        if (!empty($config['form']['sender']['email']['label'])) {
-?><label for="sender-name"><?php echo $config['form']['sender']['email']['label']; ?></label><br>
-<?php
-        }
-        ?>
-        <input
-            type="email"
-            name="sender-email"
-            id="sender-email"
-            placeholder="<?php
-                echo $config['form']['sender']['email']['palceholder'];
-            ?>"
-        >
-            </div>
-            <div class="item full-width">
-                <label for="recipient">Recipient: </label><br>
-                <div class="full-width">
-                    <select name="recipient" id="recipient">
-                        <option value="hello@e1mo.de">hello</option>
-                    </select>
-                    @e1mo.de
+        <h1><?php echo $config['page']['title']; ?></h1>
+        <p><?php echo $config['page']['description']; ?></p>
+
+        <form action="" method="post">
+            <?php
+            if (!empty($config['form']['message']['label'])) {
+    ?><label for="message"><?php echo $config['form']['message']['label']; ?></label><br>
+    <?php
+            }
+            ?>
+            <textarea
+                name="message"
+                id="message"
+                required
+                autofocus
+                placeholder="<?php
+                    echo $config['form']['message']['placeholder'];
+                ?>"
+            ><?php
+                if (!$mailSuccessCode && !empty($_POST['message'])) {
+                    echo $_POST['message'];
+                }
+            ?></textarea>
+            <br>
+            <p><?php
+                echo $config['form']['sender']['label'];
+            ?></p>
+            <div class="flex-container">
+                <div class="item">
+            <?php
+            if (!empty($config['form']['sender']['name']['label'])) {
+    ?><label for="sender-name"><?php echo $config['form']['sender']['name']['label']; ?></label><br>
+    <?php
+            }
+            ?>
+            <input
+                type="text"
+                name="sender-name"
+                id="sender-name"
+                placeholder="<?php
+                    echo $config['form']['sender']['name']['placeholder'];
+                ?>"
+            >
                 </div>
+                <div class="item">
+            <?php
+            if (!empty($config['form']['sender']['email']['label'])) {
+    ?><label for="sender-name"><?php echo $config['form']['sender']['email']['label']; ?></label><br>
+    <?php
+            }
+            ?>
+            <input
+                type="email"
+                name="sender-email"
+                id="sender-email"
+                placeholder="<?php
+                    echo $config['form']['sender']['email']['palceholder'];
+                ?>"
+            >
+                </div>
+                <!--<div class="item full-width">
+                    <label for="recipient">Recipient: </label><br>
+                    <div class="full-width">
+                        <select name="recipient" id="recipient">
+                            <option value="hello@e1mo.de">hello</option>
+                        </select>
+                        @e1mo.de
+                    </div>
+                </div>-->
             </div>
-        </div>
-        <br>
-        <input type="submit" value="<?php
-            echo $config['form']['submit']['label'];
-        ?>">
-    </form>
+            <br>
+            <input type="submit" value="<?php
+                echo $config['form']['submit']['label'];
+            ?>">
+        </form>
+    </div>
 </body>
 </html>
