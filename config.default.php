ebade7bfbbb9847d75f170a8989f34ebc75bf3cf
Moritz 'e1mo' Fromm <git@e1mo.de>
2020-10-03 20:48:09 +0200

    Add new config structure to default config

--- config.default.php
+++ config.default.php
@@ -1,65 +1,70 @@
 <?php
 
 return [
-    'page' => [
-        'language' => 'en', // two digits as of ISO 639-1
-        'title' => 'Ask me something random!',
-        'description' => 'Feel free to ask me anything. Please just be reasonably polite.',
-				'debug' => false,
-    ],
-    'owner' => [
-        'name' => '',
-        'e-mail' => 'owner@ask.example.com',
-        'recipient-choices' => [
-            'enabled' => false,
-            'choices' => [
-                //'domain1' => [
-                //    'ask', // => ask@domain1
-                //    'awesome', // => awesome@domain1
-                //]
-            ]
+  'page' => [
+    'language' => 'en', // two digits as of ISO 639-1
+    'title' => 'Ask me something random!',
+    'description' => 'Feel free to ask me anything. Please just be reasonably polite.',
+		'debug' => false,
+  ],
+  'owner' => [
+    'name' => '',
+    'e-mail' => 'owner@ask.example.com',
+    'recipient-choices' => [
+      'enabled' => false,
+      'choices' => [
+        'domains' => [
+          'myname.info',
+          'my.org',
         ],
-        'allow-bad-recipient' => false, // Set to true if you want to skip PHPMailers E-Mail validation (e.g. name only recipients for local test sendmail setup)
+        'users' => [
+          'hello',
+          'ask',
+          'awesome'
+        ]
+      ]
     ],
+    'allow-bad-recipient' => false, // Set to true if you want to skip PHPMailers E-Mail validation (e.g. name only recipients for local test sendmail setup)
+  ],
+  'message' => [
+    'subject' => 'New message for you',
+    'sender' => 'ask@example.com',
+    'force-sender' => false, // Always set it to sender even if a senders E-Mail is given
+  ],
+  'form' => [
     'message' => [
-        'subject' => 'New message for you',
-        'sender' => 'ask@example.com',
-        'force-sender' => false, // Always set it to sender even if a senders E-Mail is given
+      'label' => 'Your Message to me',
+      'placeholder' => 'I\'ve always wanted to tell you...',
     ],
-    'form' => [
-        'message' => [
-            'label' => 'Your Message to me',
-            'placeholder' => 'I\'ve always wanted to tell you...',
-        ],
-        'submit' => [
-            'label' => 'Send it!',
-        ],
-        'sender' => [
-            'label' => 'This is optional.',
-            'email' => [
-                'label' => 'Your E-Mail (optional)',
-                'palceholder' => 'person@example.com',
-            ],
-            'name' => [
-                'label' => 'Your Name (optional)',
-                'placeholder' => 'Lucky Luke',
-            ]
-        ],
-				'recipient' => [
-					'label' => 'Whom to send the message to?'
-				]
+    'submit' => [
+      'label' => 'Send it!',
     ],
-    'email' => [
-        'provider' => 'sendmail',
-        'smtp' => [
-            'host' => '',
-            'port' => 465,
-            'crypt' => 'smtps',
-            'user' => '',
-            'pass' => ''
-        ],
+    'sender' => [
+      'label' => 'This is optional.',
+      'email' => [
+        'label' => 'Your E-Mail (optional)',
+        'palceholder' => 'person@example.com',
+      ],
+      'name' => [
+        'label' => 'Your Name (optional)',
+        'placeholder' => 'Lucky Luke',
+      ]
     ],
-    'modules' => [ // Different modules that are included at different parts of the process. Must be present in modules/<name>/ and provide a main.php
-        'headers' => [] // Must return an array with all custom headers to be added
-    ]
+		'recipient' => [
+			'label' => 'Whom to send the message to?'
+		]
+  ],
+  'email' => [
+    'provider' => 'sendmail',
+    'smtp' => [
+      'host' => '',
+      'port' => 465,
+      'crypt' => 'smtps',
+      'user' => '',
+      'pass' => ''
+    ],
+  ],
+  'modules' => [ // Different modules that are included at different parts of the process. Must be present in modules/<name>/ and provide a main.php
+    'headers' => [] // Must return an array with all custom headers to be added
+  ]
 ];
